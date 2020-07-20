ae64775e580a375d8487dca9c1d98eca79793375
maquanqiang <maquanqiang@longshine.com>
2020-07-20 18:51:00 +0800

    323232323

---
+++ test-git-project1/src/com/test/demo/Test.java
@@ -0,0 +1,11 @@
+package com.test.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("66666666666666666666");
+        System.out.println("66666666666666666666");
+        System.out.println("66666666666666666666");
+        System.out.println("77777777777777777777");
+        System.out.println("88888888888888888888");
+    }
+}
---
+++ test666/src/com/test/demo/Test.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test 55555");
+        System.out.println("test 55555");
+        System.out.println("test 55555");
+        System.out.println("test 77777");
+    }
+}
